222b90a4e2674271fbfc58daae912f7591d96f68
Swati  (JSI) <swati@cleopatra.ijs.si>
2020-06-28 17:47:41 +0200

    added lines to accept non-ascii chars..

--- models/inference.py
+++ models/inference.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 def get_results(image_path, lang):
     sample = {
         'results_ir': [
--- models/sample_data.py
+++ models/sample_data.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 def load_sample_data():
     return [
         {
